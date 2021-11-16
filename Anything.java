2bc2d0d17994e71b037ed6da13e282d2a92a51da
Ramadhan Mohammed Mkoma <90829359+Ramadhanmkoma@users.noreply.github.com>
2021-11-16 05:49:44 -0800

    Create Anything.java

---
+++ Anything.java
@@ -0,0 +1,10 @@
+public class Anything {
+
+    public static void main(String[] args) {
+        int x = 24;
+        int y = 10;
+        int z = x + y;
+
+        System.out.println(z);
+    }
+}
